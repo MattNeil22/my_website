89ff74f5da3fc83fbd73443b98f71b6c67fe71d0
MattNeil22 <mneil.az@gmail.com>
2019-03-23 14:17:35 -0600

    formatting

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,8 @@
 //Select DOM Items
-const menuBtn = document.querySelector ('.menu-btn');
-const menu = document.querySelector ('.menu');
-const menuNav = document.querySelector ('.menu-nav');
-const menuBranding = document.querySelector ('.menu-branding');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
 //Set Initial State of Menu
@@ -11,7 +11,7 @@ let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
+    if (!showMenu) {
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
